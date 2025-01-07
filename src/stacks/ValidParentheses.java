cb68188de8d5a55b0a29ce63374334a3a4760811
VK132211 <m.vk.80899@gmail.com>
2025-01-07 11:08:50 +0530

    add ValidParentheses

---
+++ src/stacks/ValidParentheses.java
@@ -0,0 +1,27 @@
+package stacks;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public boolean isValid(String s) {
+		Stack<Character> st = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+				st.push(s.charAt(i));
+
+			} else {
+				if (st.isEmpty() || (s.charAt(i) == ')' && st.peek() != '(') || (s.charAt(i) == ']' && st.peek() != '[')
+						|| (s.charAt(i) == '}' && st.peek() != '{')) {
+					return false;
+				}
+				st.pop();
+
+			}
+
+		}
+
+		return st.isEmpty();
+
+	}
+}
